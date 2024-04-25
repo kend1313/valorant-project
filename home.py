5efde61a15a82492cc4c5776419cf26b47e71056
kend1313 <20086@burnside.school.nz>
2024-04-25 20:34:02 +1200

    htmls

--- home.py
+++ home.py
@@ -3,5 +3,10 @@ import sqlite3
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+# display the home page, connect with home.html
 
-print ("Hello!")
+if __name__=='__main__':
+    app.run(debug=True, port=4000)
