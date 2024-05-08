04389df2ec8a122d70889f644413a9c357b48769
kend1313 <20086@burnside.school.nz>
2024-05-08 15:28:19 +1200

    connects Sql

--- home.py
+++ home.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template # type: ignore
-import sqlite3
-db = sqlite3.connect('val.db')
-cur
 
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    db = sqlite3.connect('val.db')
+    cursor = db.cursor()
+    sql = "SELECT name,professions,origin,real_name FROM Agents;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    # close the db.
+    db.close()
+    return render_template("home.html",agents=results)
 # display the home page, connect with home.html
 
 if __name__=='__main__':
