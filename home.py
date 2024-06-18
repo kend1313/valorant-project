69e95c17d1ed0842f6b716a45044951b53a82d01
kend1313 <20086@burnside.school.nz>
2024-06-18 13:58:37 +1200

    images

--- home.py
+++ home.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 def home():
     db = sqlite3.connect('val.db')
     cursor = db.cursor()
-    sql = "SELECT Agents.name, Professions.name, Origin.name, real_name from Agents \
+    sql = "SELECT Agents.name, Professions.name, Origin.name, real_name, Agents.image from Agents \
     join Professions \
     On Agents.professions = Professions.id join Origin on Agents.origin = origin.id;"
     cursor.execute(sql)
