af7a541509de5858c67eebf5d5a20a5d6b53eadc
kend1313 <20086@burnside.school.nz>
2024-07-15 21:09:37 +1200

    new version

--- home.py
+++ home.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 # Home Route
 @app.route("/")
-def home():
+
+
+@app.route("/agents")
+def agents():
     db = sqlite3.connect('val.db')
     cursor = db.cursor()
     sql = "SELECT Agents.name, Professions.name, Origin.name, real_name, Agents.image, \
@@ -22,11 +25,11 @@ def home():
     # print them nicely.
     for val in results:
         print(val[0].encode("utf-8"))
-
-    # close the db.
     db.close()
-    return render_template("home.html",agents=results)
-# display the home page, connect with home.html
+    # close the database.
+    return render_template("agents.html",agents=results)
+# display the agents page, connect with agents.html
+
 
 if __name__=='__main__':
     app.run(debug=True, port=4000)
